a8b2ad6f8c30d4f706b3191c99fda50c8f6e1ce8
Philip Bradley <pdbradley@gmail.com>
2020-11-20 06:49:24 -0500

    yep

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,55 +1,88 @@
-import BlogLayout from 'src/layouts/BlogLayout'
 import {
   Form,
-  TextAreaField,
   TextField,
+  TextAreaField,
   Submit,
   FieldError,
   Label,
+  FormError,
 } from '@redwoodjs/forms'
-import { Link, routes } from '@redwoodjs/router'
+import { Flash, useFlash, useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const formMethods = useForm()
+  const { addMessage } = useFlash()
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      addMessage('Thank you for your submission!', {
+        style: { backgroundColor: 'green', color: 'white', padding: '1rem' },
+      })
+      formMethods.reset()
+    },
+  })
+
   const onSubmit = (data) => {
+    create({ variables: { input: data } })
     console.log(data)
   }
+
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
-        <Label errorClassName="error" name="name">
+      <Flash timeout={1000} />
+      <Form
+        onSubmit={onSubmit}
+        validation={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+        />
+        <Label name="name" errorClassName="error">
           Name
         </Label>
         <TextField
-          errorClassName="error"
           name="name"
           validation={{ required: true }}
+          errorClassName="error"
         />
         <FieldError name="name" className="error" />
-        <Label errorClassName="error" name="email">
+
+        <Label name="email" errorClassName="error">
           Email
         </Label>
         <TextField
-          errorClassName="error"
           name="email"
           validation={{
             required: true,
-            pattern: {
-              message: 'Please enter a valid email address',
-              value: /[^@]+@[^.]+\..+/,
-            },
           }}
+          errorClassName="error"
         />
         <FieldError name="email" className="error" />
-        <Label errorClassName="error" name="message">
+
+        <Label name="message" errorClassName="error">
           Message
         </Label>
         <TextAreaField
-          errorClassName="error"
           name="message"
           validation={{ required: true }}
+          errorClassName="error"
         />
         <FieldError name="message" className="error" />
-        <Submit>Save</Submit>
+
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </BlogLayout>
   )
